f4795ae92ca8d4d69df9f8a6adefe1e97bf8efe3
Jens Utbult <jens.utbult@yubico.com>
2020-06-04 10:10:18 +0200

    Added YubiKit and configured project

---
+++ SimpleOATHSwiftUI/SimpleOATHSwiftUI-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SimpleOATHSwiftUI-Bridging-Header.h
+//  SimpleOATHSwiftUI
+//
+//  Created by Jens Utbult on 2020-06-04.
+//  Copyright © 2020 Jens Utbult. All rights reserved.
+//
+
+#ifndef SimpleOATHSwiftUI_Bridging_Header_h
+#define SimpleOATHSwiftUI_Bridging_Header_h
+
+#import <YubiKit/YubiKit.h>
+
+#endif /* SimpleOATHSwiftUI_Bridging_Header_h */
